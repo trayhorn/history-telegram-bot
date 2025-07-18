e24220cb7e9fae2f1bbdd4fd9d09ddad7176b896
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-07-18 13:53:21 +0300

    Added main commands and buttons

--- app.js
+++ app.js
@@ -1,31 +1,67 @@
 import "dotenv/config";
 import TelegramBot from "node-telegram-bot-api";
 import axios from "axios";
+import cron from "node-cron";
 
 const { BOT_TOKEN: token, API_KEY } = process.env;
 
 axios.defaults.baseURL = "https://api.example.com";
 axios.defaults.headers.common["X-Api-Key"] = API_KEY;
 
+let task;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "What are we doing today?", {
+		reply_markup: {
+			keyboard: [["Schedule fact", "Cancel scheduling"], ["Random fact"]],
+			one_time_keyboard: false,
+			is_persistent: true,
+		},
+	});
+});
+
+bot.onText(/^\s*Schedule fact\s*$/i, (msg) => {
 	const chatId = msg.chat.id;
-	const text = msg.text;
 
-	if (text === "/start") {
-		bot.sendMessage(chatId, "Hey, what fact are we fetching?");
-  }
+	task = cron.schedule(
+		"*/10 * * * * *",
+		() => {
+			axios.get("https://history.muffinlabs.com/date").then(({ data }) => {
+				if (data.data.Events.length > 0) {
+					const randomFact =
+						data.data.Events[
+							Math.floor(Math.random() * data.data.Events.length)
+						];
+					const response = `
+            <b>Year:</b> ${randomFact.year}
+            \n<b>Event:</b> ${randomFact.text}
+            \n<b>Links:</b> ${randomFact.links
+							.map(({ link, title }) => {
+								return `<a href="${link}">${title}</a>`;
+							})
+							.join(", ")}`;
 
-  const url = `https://api.api-ninjas.com/v1/historicalevents?text=${text}`;
-  axios.get(url)
-    .then(({data}) => {
-      if (data.length > 0) {
-        const response = data[0].event;
-        bot.sendMessage(chatId, response);
-      } else {
-        bot.sendMessage(chatId, "Нічого не знайдено.");
-      }
-    })
+					bot.sendMessage(chatId, response, { parse_mode: "HTML" });
+				} else {
+					bot.sendMessage(chatId, "Nothing found.");
+				}
+			});
+		},
+		{ scheduled: true, timezone: "Europe/Kyiv" }
+	);
+});
+
+bot.onText(/^\s*Cancel scheduling\s*$/i, async (msg) => {
+	const chatId = msg.chat.id;
+
+	if (task) {
+		bot.sendMessage(chatId, "Scheduled task cancelled.");
+		await task.destroy();
+		task = null;
+	} else {
+		bot.sendMessage(chatId, "No scheduled task to cancel.");
+	}
 });
